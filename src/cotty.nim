d23641f0ac6de91d79b02a90dfe6590d48fc157a
zenywallet <zenywallet@bitzeny.jp>
2023-08-24 01:30:38 +0900

    add cotty not found

--- src/cotty.nim
+++ src/cotty.nim
@@ -1,9 +1,14 @@
 # Copyright (c) 2023 zenywallet
 
+import std/strformat
 import caprese
 
 server(ssl = true, ip = "0.0.0.0", port = 8009):
   routes(host = "localhost"):
-    return send("Hello!".addHeader())
+    get "/":
+      return send("Hello!".addHeader())
+
+    let urlText = sanitizeHtml(reqUrl)
+    return send(fmt"Not found: {urlText}".addDocType().addHeader(Status404))
 
 serverStart()
