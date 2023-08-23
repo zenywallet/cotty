3e9bc1c850df66ba174a1f64a196710fa304fc17
zenywallet <zenywallet@bitzeny.jp>
2023-08-24 01:57:19 +0900

    add favicon

--- src/cotty.nim
+++ src/cotty.nim
@@ -3,11 +3,15 @@
 import std/strformat
 import caprese
 import index
+import favicon
 
 server(ssl = true, ip = "0.0.0.0", port = 8009):
   routes(host = "localhost"):
     get "/":
       return response(content(CottyIndexHtml, "html"))
+
+    get "/favicon.ico":
+      return response(content(FaviconBin, "ico"))
 
     let urlText = sanitizeHtml(reqUrl)
     return send(fmt"Not found: {urlText}".addDocType().addHeader(Status404))
